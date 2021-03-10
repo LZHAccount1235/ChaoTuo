0de58230840bc31b45c738dbb72416d6c38eba7a
liuzhenhai <2420310780@qq.com>
2021-03-10 08:53:07 +0800

    第一次提交  没有核心内容

---
+++ app/src/main/java/com/example/mygittest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mygittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//https://github.com/LZHAccount1235/ChaoTuo.git
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
